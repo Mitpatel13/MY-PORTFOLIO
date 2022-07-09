8adb8406d03108cb4942d1bd3990096a53e299c0
Mit Mathukiya <54717452+Mitpatel13@users.noreply.github.com>
2022-07-09 17:21:21 +0530

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi, I'm <span className="purple">Mit Mathukiya </span>
+            from <span className="purple"> Gujarat, India.</span>
+            <br />I'm a 4th-year student, pursuing a Bachelors degree in Computer Engineering at SOU.
+            <br />
+            <br />
+            <br />
+            Apart from coding, these are some other activities that I enjoy:
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Camping
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Traveling
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Listening to Music
+            </li>
+          </ul>
+
+          <p style={{ marginBlockEnd: 0, color: "rgb(155 126 172)" }}>
+            "All repetitive, menial computer tasks can most likely be automated or partially automated with the right code."{" "}
+          </p>
+          <footer className="blockquote-footer">Mit Mathukiya</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
